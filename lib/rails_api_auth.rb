92afd1d8d4521f1b140c5f8e7b679d99bc2dd626
Oliver Azevedo Barnes <oliverbwork@gmail.com>
2015-07-10 20:54:10 +0200

    Remove unnecessary requires

--- lib/rails_api_auth.rb
+++ lib/rails_api_auth.rb
@@ -1,10 +1,4 @@
 require 'rails_api_auth/engine'
-# require 'rails'
-# require 'bcrypt'
-# require 'rack-cors'
-# require 'httparty'
-# require 'email_validator'
-# require 'faker'
 
 module RailsApiAuth
 
