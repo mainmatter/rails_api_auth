07d0c8c5efad40c1193029d3a178ee756828da11
Oliver Azevedo Barnes <oliverbwork@gmail.com>
2015-07-10 19:08:06 +0200

    Make migrations available for to application's rake db:migrate

--- lib/rails_api_auth/engine.rb
+++ lib/rails_api_auth/engine.rb
@@ -2,6 +2,14 @@ module RailsApiAuth
 
   class Engine < ::Rails::Engine
 
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths['db/migrate'].expanded.each do |expanded_path|
+          app.config.paths['db/migrate'] << expanded_path
+        end
+      end
+    end
+
     config.generators do |g|
       g.test_framework :rspec
     end
