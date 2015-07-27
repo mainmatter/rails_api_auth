248c96b4d981dbdb4f7b1938d4d7c1518becfcca
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-07-27 12:44:55 +0200

    add dummy app schema to repo

---
+++ spec/dummy/db/schema.rb
@@ -0,0 +1,27 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20150709221755) do
+
+  create_table "logins", :force => true do |t|
+    t.string   "email",                   :null => false
+    t.string   "password_digest"
+    t.string   "oauth2_token",            :null => false
+    t.string   "facebook_uid"
+    t.string   "single_use_oauth2_token"
+    t.integer  "user_id"
+    t.datetime "created_at",              :null => false
+    t.datetime "updated_at",              :null => false
+  end
+
+end
