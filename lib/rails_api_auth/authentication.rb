38809ced1e477d13be87b3c621ae390235467174
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-08-03 20:47:25 +0200

    don't memoize current login
    …as it could potentially lead to errors when the method is called multiple times with a valid token first and an invalid one later on

--- lib/rails_api_auth/authentication.rb
+++ lib/rails_api_auth/authentication.rb
@@ -12,7 +12,7 @@ module RailsApiAuth
         def authenticate!
           auth_header = request.headers['Authorization']
           token = auth_header ? auth_header.split(' ').last : ''
-          @current_login ||= Login.where(oauth2_token: token).first!
+          @current_login = Login.where(oauth2_token: token).first!
 
         rescue ActiveRecord::RecordNotFound
           head 401
