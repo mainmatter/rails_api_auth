a6ce0a75d2364540be11631e331c35f3832d26b1
Paulo Fidalgo <fidalgo@gmx.com>
2015-09-07 10:51:59 +0100

    Fix rubocop offensions.

--- app/services/google_authenticator.rb
+++ app/services/google_authenticator.rb
@@ -27,52 +27,55 @@ class GoogleAuthenticator
 
   private
 
-  def login
-    @login ||= Login.where(identification: google_user[:email]).first
-  end
-
-  def connect_login_to_google_account
-    login.update_attributes!(uid: google_user[:sub], provider: PROVIDER)
-  end
-
-  def create_login_from_google_account
-    login_attributes = {
-      identification: google_user[:email],
-      uid: google_user[:sub],
-      provider: PROVIDER
-    }
-
-    @login = Login.create!(login_attributes)
-  end
-
-  def google_user
-    @google_user ||= begin
-      get_request(user_url(access_token)).parsed_response.symbolize_keys
+    def login
+      @login ||= Login.where(identification: google_user[:email]).first
     end
-  end
 
-  def get_request(url)
-    response = HTTParty.get(url)
-    raise ApiError.new if response.code != 200
-    response
-  end
+    def connect_login_to_google_account
+      login.update_attributes!(uid: google_user[:sub], provider: PROVIDER)
+    end
 
-  def user_url(access_token)
-    "#{PROFILE_URL}#{access_token}"
-  end
-
-  def access_token
-    options = {
-      body: {
-        code: @auth_code,
-        client_id: RailsApiAuth.google_client_id,
-        client_secret: RailsApiAuth.google_client_secret,
-        redirect_uri: RailsApiAuth.google_redirect_uri,
-        grant_type: 'authorization_code'
+    def create_login_from_google_account
+      login_attributes = {
+        identification: google_user[:email],
+        uid: google_user[:sub],
+        provider: PROVIDER
       }
-    }
-    response = HTTParty.post(TOKEN_URL, options)
-    response.parsed_response['access_token']
-  end
+
+      @login = Login.create!(login_attributes)
+    end
+
+    def google_user
+      @google_user ||= begin
+        get_request(user_url(access_token)).parsed_response.symbolize_keys
+      end
+    end
+
+    def get_request(url)
+      response = HTTParty.get(url)
+      raise ApiError.new if response.code != 200
+      response
+    end
+
+    def user_url(access_token)
+      "#{PROFILE_URL}#{access_token}"
+    end
+
+    def access_token
+      response = HTTParty.post(TOKEN_URL, @token_options)
+      response.parsed_response['access_token']
+    end
+
+    def token_options
+      @token_options ||= {
+        body: {
+          code: @auth_code,
+          client_id: RailsApiAuth.google_client_id,
+          client_secret: RailsApiAuth.google_client_secret,
+          redirect_uri: RailsApiAuth.google_redirect_uri,
+          grant_type: 'authorization_code'
+        }
+      }
+    end
 
 end
--- db/migrate/20150904110438_add_provider_to_login.rb
+++ db/migrate/20150904110438_add_provider_to_login.rb
@@ -1,6 +1,8 @@
 class AddProviderToLogin < ActiveRecord::Migration
+
   def change
     add_column :logins, :provider, :string
     rename_column :logins, :facebook_uid, :uid
   end
+
 end
