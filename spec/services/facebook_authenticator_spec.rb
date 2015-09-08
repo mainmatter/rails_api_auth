0f069a8193ad1e21d6aadee30862384d6d05c23c
Oliver Azevedo Barnes <oliver.barnes@simplabs.com>
2015-09-08 12:56:54 +0100

    Dry up tests with a shared context for stubbed facebook requests

--- spec/services/facebook_authenticator_spec.rb
+++ spec/services/facebook_authenticator_spec.rb
@@ -1,24 +1,17 @@
 describe FacebookAuthenticator do
+  include_context 'stubbed facebook requests'
+
   describe '#authenticate!' do
-    let(:auth_code) { 'authcode' }
-    let(:email)     { 'email@facebook.com' }
     let(:facebook_data) do
       {
         id:    '1238190321',
         email: email
       }
     end
-    let(:access_token)           { 'CAAMvEGOZAxB8BAODGpIWO9meEXEpvigfIRs5j7LIi1Uef8xvTz4vpayfP6rxn0Om3jZAmvEojZB9HNWD44PgSSwFyD7bKsJ3EaNMKwYpZBRqjm25HfwUzF3pOVRXp9cdquT1afm7bj4mnb4WFFo7TxLcgO848FaAKZBdxwefJlPneVUSpquEh2TZAVWghndnPO9ON7QTqXhAZDZD' }
-    let(:response_with_fb_token) { { body: "access_token=#{access_token}&expires=5175490" } }
-    let(:response_with_fb_user)  { { body: JSON.generate(facebook_data), headers: { 'Content-Type' => 'application/json' } } }
-    let(:login)                  { double('login') }
+    let(:email) { 'email@facebook.com' }
+    let(:login) { double('login') }
 
     subject { described_class.new(auth_code).authenticate! }
-
-    before do
-      stub_request(:get, 'https://graph.facebook.com/oauth/access_token?client_id=app_id&client_secret=app_secret&code=authcode&redirect_uri=redirect_uri').to_return(response_with_fb_token)
-      stub_request(:get, "https://graph.facebook.com/me?fields=email,name&access_token=#{access_token}").to_return(response_with_fb_user)
-    end
 
     context 'when no login for the Facebook account exists' do
       let(:login_attributes) do
