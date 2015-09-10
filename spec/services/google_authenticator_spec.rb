302ad4a90407198b7848a11282cb206ee5663141
Paulo Fidalgo <fidalgo@gmx.com>
2015-09-10 16:22:15 +0100

    Make use of shared examples for stubbed google requests

--- spec/services/google_authenticator_spec.rb
+++ spec/services/google_authenticator_spec.rb
@@ -1,25 +1,18 @@
 describe GoogleAuthenticator do
+  include_context 'stubbed google requests'
   describe '#authenticate!' do
     let(:provider) { 'google' }
-    let(:auth_code) { 'authcode' }
-    let(:email)     { 'email@gmail.com' }
+    let(:email) { 'email@gmail.com' }
     let(:google_data) do
       {
         sub: '1238190321',
         email: email
       }
     end
-    let(:response_with_token) { { body: '{ "access_token": "access_token" },  "token_type": "Bearer", "expires_in": 3600' } }
-    let(:response_with_user)  { { body: JSON.generate(google_data), headers: { 'Content-Type' => 'application/json' } } }
+
     let(:login) { double('login') }
 
     subject { described_class.new(auth_code).authenticate! }
-
-    before do
-      stub_request(:post, described_class::TOKEN_URL).
-        with(body: hash_including(grant_type: 'authorization_code')).to_return(response_with_token)
-      stub_request(:get, described_class::PROFILE_URL % { access_token: 'access_token' }).to_return(response_with_user)
-    end
 
     context 'when no login for the Google account exists' do
       let(:login_attributes) do
