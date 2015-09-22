9efa676b64a5df94449f065103bc59dca0b7d496
Paulo Fidalgo <fidalgo@gmx.com>
2015-09-22 21:21:09 +0100

    Dry the test specs for Oauth2
    The test scenarios when using 'facebook_auth_code' or 'google_auth_code
    as grant_type are similar, so a new shared_context was creating to address
    both cases.

--- spec/requests/oauth2_spec.rb
+++ spec/requests/oauth2_spec.rb
@@ -39,179 +39,31 @@ describe 'Oauth2 API' do
     end
 
     context 'for grant_type "facebook_auth_code"' do
-      include_context 'stubbed facebook requests'
-
-      let(:params)                { { grant_type: 'facebook_auth_code', auth_code: 'authcode' } }
-      let(:facebook_email)        { login.identification }
       let(:authenticated_user_data) do
         {
           id:    '1238190321',
-          email: facebook_email
+          email: email
         }
       end
-
-      context 'when a login with for the Facebook account exists' do
-        it 'connects the login to the Facebook account' do
-          subject
-
-          expect(login.reload.uid).to eq(authenticated_user_data[:id])
-        end
-
-        it 'responds with status 200' do
-          subject
-
-          expect(response).to have_http_status(200)
-        end
-
-        it "responds with the login's OAuth 2.0 token" do
-          subject
-
-          expect(response.body).to be_json_eql({ access_token: login.oauth2_token }.to_json)
-        end
-      end
-
-      context 'when no login for the Facebook account exists' do
-        let(:facebook_email) { Faker::Internet.email }
-
-        it 'responds with status 200' do
-          subject
-
-          expect(response).to have_http_status(200)
-        end
-
-        it 'creates a login for the Facebook account' do
-          expect { subject }.to change { Login.where(identification: facebook_email).count }.by(1)
-        end
-
-        it "responds with the login's OAuth 2.0 token" do
-          subject
-          login = Login.where(identification: facebook_email).first
-
-          expect(response.body).to be_json_eql({ access_token: login.oauth2_token }.to_json)
-        end
-      end
-
-      context 'when no Facebook auth code is sent' do
-        let(:params) { { grant_type: 'facebook_auth_code' } }
-
-        it 'responds with status 400' do
-          subject
-
-          expect(response).to have_http_status(400)
-        end
-
-        it 'responds with a "no_authorization_code" error' do
-          subject
-
-          expect(response.body).to be_json_eql({ error: 'no_authorization_code' }.to_json)
-        end
-      end
-
-      context 'when Facebook responds with an error' do
-        before do
-          stub_request(:get, FacebookAuthenticator::PROFILE_URL % { access_token: access_token }).to_return(status: 422)
-        end
-
-        it 'responds with status 502' do
-          subject
-
-          expect(response).to have_http_status(502)
-        end
-
-        it 'responds with an empty response body' do
-          subject
-
-          expect(response.body.strip).to eql('')
-        end
-      end
+      let(:uid_mapped_field) { 'id' }
+      let(:grant_type) { 'facebook_auth_code' }
+      let(:profile_url) { FacebookAuthenticator::PROFILE_URL }
+      include_context 'stubbed facebook requests'
+      it_behaves_like 'oauth2 shared contexts'
     end
 
     context 'for grant_type "google_auth_code"' do
-      include_context 'stubbed google requests'
-
-      let(:params) { { grant_type: 'google_auth_code', auth_code: 'authcode' } }
-      let(:email) { login.identification }
       let(:authenticated_user_data) do
         {
           sub: '1238190321',
           email: email
         }
       end
-
-      context 'when a login with email for the Google account exists' do
-        it 'connects the login to the Google account' do
-          subject
-
-          expect(login.reload.uid).to eq(authenticated_user_data[:sub])
-        end
-
-        it 'responds with status 200' do
-          subject
-
-          expect(response).to have_http_status(200)
-        end
-
-        it "responds with the login's OAuth 2.0 token" do
-          subject
-
-          expect(response.body).to be_json_eql({ access_token: login.oauth2_token }.to_json)
-        end
-      end
-
-      context 'when no login for the Google account exists' do
-        let(:email) { Faker::Internet.email }
-
-        it 'responds with status 200' do
-          subject
-
-          expect(response).to have_http_status(200)
-        end
-
-        it 'creates a login for the Gmail account' do
-          expect { subject }.to change { Login.where(identification: email).count }.by(1)
-        end
-
-        it "responds with the login's OAuth 2.0 token" do
-          subject
-          login = Login.where(identification: email).first
-
-          expect(response.body).to be_json_eql({ access_token: login.oauth2_token }.to_json)
-        end
-      end
-
-      context 'when no Google auth code is sent' do
-        let(:params) { { grant_type: 'google_auth_code' } }
-
-        it 'responds with status 400' do
-          subject
-
-          expect(response).to have_http_status(400)
-        end
-
-        it 'responds with a "no_authorization_code" error' do
-          subject
-
-          expect(response.body).to be_json_eql({ error: 'no_authorization_code' }.to_json)
-        end
-      end
-
-      context 'when Google responds with an error' do
-        before do
-          stub_request(:get, GoogleAuthenticator::PROFILE_URL % { access_token: 'access_token' }).to_return(status: 422)
-        end
-
-        it 'responds with status 502' do
-          subject
-
-          expect(response).to have_http_status(502)
-        end
-
-        it 'responds with an empty response body' do
-          subject
-
-          expect(response.body.strip).to eql('')
-        end
-      end
+      let(:uid_mapped_field) { 'sub' }
+      let(:grant_type) { 'google_auth_code' }
+      let(:profile_url) { GoogleAuthenticator::PROFILE_URL }
+      include_context 'stubbed google requests'
+      it_behaves_like 'oauth2 shared contexts'
     end
 
     context 'for an unknown grant type' do
