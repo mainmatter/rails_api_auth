2faa20a6d953d56e936fdaa07ee16713f5123abd
williamcoates <william.coates@gmail.com>
2015-10-15 14:58:17 +0100

    Fix engine name
    rails_api_auth_engine (default) -> rails_api_auth
    
    This means if you are e.g installing migrations you can run
    
        rake rails_api_auth:install:migrations
    
    As apposed to rake rails_api_auth_engine:install:migrations which you
    probably wouldn’t guess.

--- lib/rails_api_auth/engine.rb
+++ lib/rails_api_auth/engine.rb
@@ -3,6 +3,8 @@ module RailsApiAuth
   # @!visibility private
   class Engine < ::Rails::Engine
 
+    engine_name 'rails_api_auth'
+
     initializer :append_migrations do |app|
       unless app.root.to_s.match root.to_s
         config.paths['db/migrate'].expanded.each do |expanded_path|
