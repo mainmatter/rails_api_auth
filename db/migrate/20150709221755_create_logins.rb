2e409ec48092cfc206d62d3157a16049f85b50f0
williamcoates <william.coates@gmail.com>
2015-10-15 14:49:20 +0100

    Use method instead of closure
    Was trying so hard to find a nice one liner that I kind of missed the
    obvious approach :)

--- db/migrate/20150709221755_create_logins.rb
+++ db/migrate/20150709221755_create_logins.rb
@@ -1,20 +1,23 @@
 class CreateLogins < ActiveRecord::Migration
 
   def change
-    primary_key_chooser = lambda do |key|
-      RailsApiAuth.primary_key_type ? { key => RailsApiAuth.primary_key_type } : {}
-    end
-    create_table :logins, primary_key_chooser.call(:id) do |t|
+    create_table :logins, primary_key_options(:id) do |t|
       t.string :identification,  null: false
       t.string :password_digest, null: true
       t.string :oauth2_token,    null: false
       t.string :facebook_uid
       t.string :single_use_oauth2_token
 
-      t.references :user, primary_key_chooser.call(:type)
+      t.references :user, primary_key_options(:type)
 
       t.timestamps
     end
+  end
+
+  private
+
+  def primary_key_options(option_name)
+    RailsApiAuth.primary_key_type ? { option_name => RailsApiAuth.primary_key_type } : {}
   end
 
 end
