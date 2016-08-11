654c76837de4e6008f8d759409e879ddbac247ab
epattermann <epattermann@moneythink.org>
2016-08-11 12:07:47 -0500

    logins are distinguished by provider

--- app/controllers/oauth2_controller.rb
+++ app/controllers/oauth2_controller.rb
@@ -5,6 +5,8 @@ require 'login_not_found'
 # @!visibility private
 class Oauth2Controller < ApplicationController
 
+  PASSWORD_PROVIDER = 'password'.freeze
+
   force_ssl if: -> { RailsApiAuth.force_ssl }
 
   # rubocop:disable MethodLength
@@ -35,8 +37,8 @@ class Oauth2Controller < ApplicationController
 
   private
 
-    def authenticate_with_credentials(identification, password)
-      login = Login.where(identification: identification).first || LoginNotFound.new
+    def authenticate_with_credentials(identification, password, provider = PASSWORD_PROVIDER)
+      login = Login.where(provider: provider, identification: identification).first || LoginNotFound.new
 
       if login.authenticate(password)
         render json: { access_token: login.oauth2_token }
