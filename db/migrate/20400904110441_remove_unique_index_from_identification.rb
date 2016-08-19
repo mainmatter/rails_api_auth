d0bcf65c495cc1edf38f3c09d03a01805fa1fb8b
epattermann <epattermann@moneythink.org>
2016-08-19 09:51:23 -0500

    removed unnecessary param

--- db/migrate/20400904110441_remove_unique_index_from_identification.rb
+++ db/migrate/20400904110441_remove_unique_index_from_identification.rb
@@ -2,7 +2,7 @@ class RemoveUniqueIndexFromIdentification < ActiveRecord::Migration
 
   def change
     remove_index :logins, :identification
-    add_index :logins, :identification, using: :btree
+    add_index :logins, :identification
   end
 
 end
